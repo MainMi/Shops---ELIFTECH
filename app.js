bd57a6491045f9af666a8e3c2bae28cbcd4fc0e9
MainMi <miksmikita7@gmail.com>
2022-07-07 23:51:57 +0300

    create app

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const expressHbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const { PORT, MONGO_URL } = require('./config/config');
+const { apiRouter } = require('./router');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.set('view engine', '.hbs');
+
+app.engine('.hbs', expressHbs.engine({
+    defaultLayout: false
+}));
+
+app.set('views', path.join(__dirname, './static'));
+
+app.use('/', apiRouter);
+
+function _connectDB() {
+    mongoose.connect(MONGO_URL, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    });
+}
+
+_connectDB();
+
+app.listen(PORT, () => {
+    console.log(`start server ${PORT}`);
+});
